cfbe80f5a585211083891004d5cb88bf050a8694
Olutobi <olutobi.akinsinmide@edouardgand.fr>
2026-03-25 14:17:08 +0100

    Ajout test de type casting et securisation du casting

--- Program.cs
+++ Program.cs
@@ -24,27 +24,35 @@ class Program
 
         m1.Afficher();
         Console.WriteLine($"Après dégats: {m1.RecevoirDegats(50)}");
-        Console.WriteLine($"Après réduction et dégats: {m1.RecevoirDegats(50,20)}");
+        Console.WriteLine($"Après réduction et dégats: {m1.RecevoirDegats(50, 20)}");
 
         m1.LancerSort();
         g1.Attaquer();
 
         //creation de liste de personnage p7
         Console.WriteLine("=====polymorph test======");
-        List <Personnage> personnages = new List<Personnage>();
+        List<Personnage> personnages = new List<Personnage>();
         personnages.Add(new Magicien("Loko le magicien", 300, 100));
-        personnages.Add(new Guerrier("Hluk le guerrier",1500,500));
+        personnages.Add(new Guerrier("Hluk le guerrier", 1500, 500));
 
 
-        foreach(Personnage personnage in personnages)
+        foreach (Personnage personnage in personnages)
         {
             personnage.Afficher();
             //teste avec une autr méthode p8
-           // personnage.Attaquer(); //erreur de compilation
-           
-        Console.WriteLine("=====polymorph foreach test======");
+            // personnage.Attaquer(); //erreur de compilation
 
-           if(personnage is Magicien m)
+            Console.WriteLine("=====polymorph foreach test======");
+
+            //casting sans securisation
+            
+            // Guerrier g = (Guerrier)personnage;
+            // g.Attaquer();
+
+            // Magicien m = (Magicien)personnage;
+            // m.LancerSort();
+
+            if (personnage is Magicien m)
             {
                 m.LancerSort();
             }
